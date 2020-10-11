2fcf93e46b8dd89926a34a9399d3bf27cb4d4c55
jihate12 <ariefshitery@gmail.com>
2020-10-11 23:38:29 +0700

    assessment 1

---
+++ ujian_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+angka=
+nama=
+echo "Masukan nama : "
+ read nama
+echo "Masukan jumlalh perulangan : "
+read angka
+for (( i=1; i<="$angka"; i++ ))
+do
+     echo "Halo, $nama"
+ done
+ echo "Nama anda di ulang sebanyak $angka kali"
