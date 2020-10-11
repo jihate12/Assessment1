a56ab42a5a801eb8b56a16b95e9acd242d002e64
jihate12 <ariefshitery@gmail.com>
2020-10-11 23:44:29 +0700

    assessment 1

--- ujian_1.sh
+++ ujian_1.sh
@@ -2,11 +2,12 @@
 angka=
 nama=
 echo "Masukan nama : "
- read nama
+read nama
 echo "Masukan jumlalh perulangan : "
 read angka
 for (( i=1; i<="$angka"; i++ ))
 do
      echo "Halo, $nama"
  done
+ echo " "
  echo "Nama anda di ulang sebanyak $angka kali"
